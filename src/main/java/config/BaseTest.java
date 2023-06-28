a521cad55adfdef8960a610c4d6be57f9f2d8d71
VrublevskiyVolodymyr <115405839+VrublevskiyVolodymyr@users.noreply.github.com>
2023-06-28 20:05:06 +0300

    FirstTest

--- src/main/java/config/BaseTest.java
+++ src/main/java/config/BaseTest.java
@@ -2,15 +2,16 @@ package config;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeMethod;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import io.github.bonigarcia.wdm.managers.ChromeDriverManager;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriverService;
+import org.testng.annotations.*;
 
 public class BaseTest {
 
     @BeforeClass(alwaysRun = true)
-    public void configuration(){
+    public void configuration() {
         Configuration.browser = "chrome";
         Configuration.startMaximized = false;
 //        Configuration.browserSize = "300x200";
@@ -22,21 +23,29 @@ public class BaseTest {
         Configuration.savePageSource = false;
         Configuration.pageLoadTimeout = 10000;
     }
+
     @BeforeMethod
-    public void setUp(){
+    public void setUp() {
         Selenide.open("https://www.google.com/");
     }
 
     @AfterMethod(alwaysRun = true)
-    public void cleanWebDriver(){
+    public void cleanWebDriver() {
         Selenide.clearBrowserCookies();
         Selenide.refresh();
         Selenide.open("about:blank");
     }
+
     @AfterClass
-    public void tearDown(){
+    public void tearDown() {
         Selenide.closeWebDriver();
     }
 
+    @AfterSuite
+    public void tearDownSuite() {
+        ChromeDriver driver = new ChromeDriver();
+        driver.quit();//закриває хромдрайвер після відпрацювання
+    }
 
 }
+
